b6f934006e64ad65fb3548c06684ce7d895bed8e
Rishi <rishikanchi@programmer.net>
2022-03-15 07:32:20 -0500

    Dihybrid Cross Functoin Completed

--- script.js
+++ script.js
@@ -17,8 +17,38 @@ function monoCross(paternal, maternal, total, observedArray){
     return output;
 }
 
-function diCross(){
+function diCross(patTrait1, matTrait1, patTrait2, matTrait2, total, observedArray){
+    patTrait1 = charToNum(patTrait1);
+    matTrait1 = charToNum(matTrait1);
+    patTrait2 = charToNum(patTrait2);
+    matTrait2 = charToNum(matTrait2);
+
+    var t1pheno = getPhenotype(patTrait1, matTrait1);
+    var t2pheno = getPhenotype(patTrait2, matTrait2);
+
+    var fullPheno = [];
+    var t1Ratio;
+    var t2Ratio;
+    for (let i=0; i<2; i++){
+        t1Ratio = t1pheno[i];
+        for (let j=0; j<2; j++){
+            t2Ratio = t2pheno[j];
+            fullPheno.push(t1Ratio*t2Ratio);
+        }
+    }
+
+    var expectedArray =[];
+    for (let i=0; i<4; i++){
+        const expected =  (fullPheno[i]/16)*total;
+        expectedArray.push(expected);
+     }
+
+    var output = 0;
+    for (let i=0; i<4; i++){
+        output += chiSquareEq(observedArray[i], expectedArray[i]);
+    }
     
+    return output;
 }
 
 function getPhenotype(paternal, maternal){
@@ -71,7 +101,7 @@ function displayTextBox(){
         document.getElementById('textBox').innerHTML = "<input type='text' id='box1' placeholder='Paternal Genotype'>x<input type='text' id='box2' placeholder='Maternal Genotype'> <br><br>Observed <br>Total: <input type='text' id='total' placeholder='Total'> Dominant: <input type='text' id='dominant' placeholder='Dominant'> Recessive: <input type='text' id='recessive' placeholder='Recessive'>";
     } 
     else if (selected == '2') {
-       document.getElementById('textBox').innerHTML = "<input type='text' id='box1' placeholder='Paternal Genotype'>x<input type='text' id='box2' placeholder='Maternal Genotype'><br><input type='text' id='box3' placeholder='Paternal Genotype'>x<input type='text' id='box4' placeholder='Maternal Genotype'><br><br>Observed <br>Total: <input type='text' id='total' placeholder='total'> Dominant Dominant: <input type='text' id='domDom' placeholder='Dominant Dominant'> Dominant Recessive: <input type='text' id='domRes' placeholder='Dominant Recessive'>Recessive Dominant: <input type='text' id='recDom' placeholder='Recessive Dominant'>Recessive Recessive: <input type='text' id='recRec' placeholder='Recessive Recessive'>";
+       document.getElementById('textBox').innerHTML = "Trait 1: <input type='text' id='box1' placeholder='Paternal Genotype'>x<input type='text' id='box2' placeholder='Maternal Genotype'><br>Trait 2: <input type='text' id='box3' placeholder='Paternal Genotype'>x<input type='text' id='box4' placeholder='Maternal Genotype'><br><br>Observed <br>Total: <input type='text' id='total' placeholder='total'> Dominant Dominant: <input type='text' id='domDom' placeholder='Dominant Dominant'> Dominant Recessive: <input type='text' id='domRec' placeholder='Dominant Recessive'>Recessive Dominant: <input type='text' id='recDom' placeholder='Recessive Dominant'>Recessive Recessive: <input type='text' id='recRec' placeholder='Recessive Recessive'>";
     }
 }
 
@@ -91,6 +121,10 @@ function calcClick(){
     else if (selected == "2"){
         const paternal1 = document.getElementById('box3').value;
         const maternal1 = document.getElementById('box4').value;
-        document.getElementById('output').innerHTML = diCross(paternal, maternal, paternal1, maternal1);  
+        const domDom = document.getElementById('domDom').value;
+        const domRec = document.getElementById('domRec').value;
+        const recDom = document.getElementById('recDom').value;
+        const recRec = document.getElementById('recRec').value;
+        document.getElementById('output').innerHTML = diCross(paternal, maternal, paternal1, maternal1, total, [domDom, domRec, recDom, recRec]);  
     }
 }
